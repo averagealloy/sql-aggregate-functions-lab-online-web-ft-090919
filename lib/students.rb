2da23e9910bd09ae7b100c9d52896f74302878f3
Mike McTighe <m.mctighe10@gmail.com>
2019-10-10 17:02:17 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -13,7 +13,7 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-  "SELECT SU(*) WHERE tardies > 0 FROM students;"
+  "SELECT SUM() WHERE tardies > 0 FROM students;"
 end
 
 def average_gpa_for_9th_grade
