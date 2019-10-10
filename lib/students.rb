56aefd97354c7e82f514a4ce94ae1d4aa94a4836
Mike McTighe <m.mctighe10@gmail.com>
2019-10-10 15:31:48 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -13,7 +13,7 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-  "SELECT COUNT(gpa) AS average_student_gpa FROM studentsnot trying to do story hour w;"
+  "SELECT COUNT(ta) AS average_student_gpa FROM studentsnot trying to do story hour w;"
 end
 
 def average_gpa_for_9th_grade
