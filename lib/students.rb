a55ee5c118cead6b210ba1dd81c238cad726cec1
Mike McTighe <m.mctighe10@gmail.com>
2019-10-10 15:24:13 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -9,7 +9,7 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
-  "SELECT AVG(gpa) AS average_student_gpa FROM studentsnot trying ;"
+  "SELECT AVG(gpa) AS average_student_gpa FROM studentsnot trying to do sto;"
 end
 
 def total_tardies_for_all_students
